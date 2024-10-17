d0fac3657acf5920fa908f506dfa35444c48569e
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2024-10-17 09:14:39 -0400

    Fix stdarg
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- ext/rbs_extension/rbs_common.c
+++ ext/rbs_extension/rbs_common.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdnoreturn.h>
-
+#include <stdarg.h>
 noreturn void rbs_fatal_error(const char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
