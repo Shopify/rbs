693c75d93e6164ac3773ed7d69d51d4cdd4ef313
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2024-10-18 14:58:56 -0400

    WIP: generate nodes
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- ext/rbs_extension/extconf.rb
+++ ext/rbs_extension/extconf.rb
@@ -1,4 +1,14 @@
 require 'mkmf'
+
+root_dir = File.expand_path('../../../', __FILE__)
+
+$srcs = Dir.glob("#{root_dir}/src/*.c") +
+        Dir.glob("#{root_dir}/ext/rbs_extension/*.c")
+
 $INCFLAGS << " -I$(top_srcdir)" if $extmk
+
+$VPATH << "#{root_dir}/src"
+$VPATH << "#{root_dir}/ext/rbs_extension"
+
 append_cflags ['-std=gnu99']
 create_makefile 'rbs_extension'
---
+++ ext/rbs_extension/ruby_objs_old.c
@@ -0,0 +1,97 @@
+#include "rbs_extension.h"
+
+#ifdef RB_PASS_KEYWORDS
+  // Ruby 2.7 or later
+  #define CLASS_NEW_INSTANCE(klass, argc, argv)\
+          rb_class_new_instance_kw(argc, argv, klass, RB_PASS_KEYWORDS)
+#else
+  // Ruby 2.6
+  #define CLASS_NEW_INSTANCE(receiver, argc, argv)\
+          rb_class_new_instance(argc, argv, receiver)
+#endif
+
+VALUE rbs_base_type(VALUE klass, VALUE location) {
+  VALUE args = rb_hash_new();
+  rb_hash_aset(args, ID2SYM(rb_intern("location")), location);
+
+  return CLASS_NEW_INSTANCE(
+    klass,
+    1,
+    &args
+  );
+}
+
+VALUE rbs_ast_type_param(VALUE name, VALUE variance, bool unchecked, VALUE upper_bound, VALUE default_type, VALUE location) {
+  VALUE args = rb_hash_new();
+  rb_hash_aset(args, ID2SYM(rb_intern("name")), name);
+  rb_hash_aset(args, ID2SYM(rb_intern("variance")), variance);
+  rb_hash_aset(args, ID2SYM(rb_intern("upper_bound")), upper_bound);
+  rb_hash_aset(args, ID2SYM(rb_intern("default_type")), default_type);
+  rb_hash_aset(args, ID2SYM(rb_intern("location")), location);
+
+  VALUE type_param = CLASS_NEW_INSTANCE(RBS_AST_TypeParam, 1, &args);
+
+  if (unchecked) {
+    rb_funcall(type_param, rb_intern("unchecked!"), 0);
+  }
+
+  return type_param;
+}
+
+VALUE rbs_ast_members_variable(VALUE klass, VALUE name, VALUE type, VALUE location, VALUE comment) {
+  VALUE args = rb_hash_new();
+  rb_hash_aset(args, ID2SYM(rb_intern("name")), name);
+  rb_hash_aset(args, ID2SYM(rb_intern("type")), type);
+  rb_hash_aset(args, ID2SYM(rb_intern("location")), location);
+  rb_hash_aset(args, ID2SYM(rb_intern("comment")), comment);
+
+  return CLASS_NEW_INSTANCE(
+    klass,
+    1,
+    &args
+  );
+}
+
+VALUE rbs_ast_members_mixin(VALUE klass, VALUE name, VALUE module_args, VALUE annotations, VALUE location, VALUE comment) {
+  VALUE args = rb_hash_new();
+  rb_hash_aset(args, ID2SYM(rb_intern("name")), name);
+  rb_hash_aset(args, ID2SYM(rb_intern("args")), module_args);
+  rb_hash_aset(args, ID2SYM(rb_intern("annotations")), annotations);
+  rb_hash_aset(args, ID2SYM(rb_intern("location")), location);
+  rb_hash_aset(args, ID2SYM(rb_intern("comment")), comment);
+
+  return CLASS_NEW_INSTANCE(
+    klass,
+    1,
+    &args
+  );
+}
+
+VALUE rbs_ast_members_attribute(VALUE klass, VALUE name, VALUE type, VALUE ivar_name, VALUE kind, VALUE annotations, VALUE location, VALUE comment, VALUE visibility) {
+  VALUE args = rb_hash_new();
+  rb_hash_aset(args, ID2SYM(rb_intern("name")), name);
+  rb_hash_aset(args, ID2SYM(rb_intern("type")), type);
+  rb_hash_aset(args, ID2SYM(rb_intern("ivar_name")), ivar_name);
+  rb_hash_aset(args, ID2SYM(rb_intern("kind")), kind);
+  rb_hash_aset(args, ID2SYM(rb_intern("annotations")), annotations);
+  rb_hash_aset(args, ID2SYM(rb_intern("location")), location);
+  rb_hash_aset(args, ID2SYM(rb_intern("comment")), comment);
+  rb_hash_aset(args, ID2SYM(rb_intern("visibility")), visibility);
+
+  return CLASS_NEW_INSTANCE(
+    klass,
+    1,
+    &args
+  );
+}
+
+VALUE rbs_ast_members_visibility(VALUE klass, VALUE location) {
+  VALUE args = rb_hash_new();
+  rb_hash_aset(args, ID2SYM(rb_intern("location")), location);
+
+  return CLASS_NEW_INSTANCE(
+    klass,
+    1,
+    &args
+  );
+}
