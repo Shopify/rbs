4493491e5157ff28d4e60d1d05009463861af78a
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2024-10-29 10:06:35 -0400

    Generate RBS::AST::Declarations::Module node
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- ext/rbs_extension/constants.h
+++ ext/rbs_extension/constants.h
@@ -5,7 +5,6 @@ extern VALUE RBS_AST_TypeParam;
 
 extern VALUE RBS_AST_Declarations_TypeAlias;
 extern VALUE RBS_AST_Declarations_Module_Self;
-extern VALUE RBS_AST_Declarations_Module;
 extern VALUE RBS_AST_Declarations_ModuleAlias;
 
 extern VALUE RBS_AST_Directives_Use;
