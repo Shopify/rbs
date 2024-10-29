2e5e5f4d2ac05557d2ba7795a5587fc70c179670
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2024-10-29 10:04:32 -0400

    Generate RBS::AST::Declarations::Global node
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- ext/rbs_extension/ruby_objs.c
+++ ext/rbs_extension/ruby_objs.c
@@ -300,20 +300,6 @@ VALUE rbs_ast_type_param(VALUE name, VALUE variance, bool unchecked, VALUE upper
   return type_param;
 }
 
-VALUE rbs_ast_decl_global(VALUE name, VALUE type, VALUE location, VALUE comment) {
-  VALUE args = rb_hash_new();
-  rb_hash_aset(args, ID2SYM(rb_intern("name")), name);
-  rb_hash_aset(args, ID2SYM(rb_intern("type")), type);
-  rb_hash_aset(args, ID2SYM(rb_intern("location")), location);
-  rb_hash_aset(args, ID2SYM(rb_intern("comment")), comment);
-
-  return CLASS_NEW_INSTANCE(
-    RBS_AST_Declarations_Global,
-    1,
-    &args
-  );
-}
-
 VALUE rbs_ast_decl_type_alias(VALUE name, VALUE type_params, VALUE type, VALUE annotations, VALUE location, VALUE comment) {
   VALUE args = rb_hash_new();
   rb_hash_aset(args, ID2SYM(rb_intern("name")), name);
