0a7c45d41eaf60ebf3df1b660a8df905f073e727
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2024-10-29 10:31:39 -0400

    Generate RBS::Types::* nodes
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- ext/rbs_extension/constants.c
+++ ext/rbs_extension/constants.c
@@ -1,57 +1,6 @@
 #include "rbs_extension.h"
 
-VALUE RBS_Types_Alias;
-VALUE RBS_Types_Bases_Any;
-VALUE RBS_Types_Bases_Bool;
-VALUE RBS_Types_Bases_Bottom;
-VALUE RBS_Types_Bases_Class;
-VALUE RBS_Types_Bases_Instance;
-VALUE RBS_Types_Bases_Nil;
-VALUE RBS_Types_Bases_Self;
-VALUE RBS_Types_Bases_Top;
-VALUE RBS_Types_Bases_Void;
-VALUE RBS_Types_Block;
-VALUE RBS_Types_ClassInstance;
-VALUE RBS_Types_ClassSingleton;
-VALUE RBS_Types_Function_Param;
-VALUE RBS_Types_Function;
-VALUE RBS_Types_UntypedFunction;
-VALUE RBS_Types_Interface;
-VALUE RBS_Types_Intersection;
-VALUE RBS_Types_Literal;
-VALUE RBS_Types_Optional;
-VALUE RBS_Types_Proc;
-VALUE RBS_Types_Record;
-VALUE RBS_Types_Tuple;
-VALUE RBS_Types_Union;
-VALUE RBS_Types_Variable;
-
 #define IMPORT_CONSTANT(var, parent, name) { var = rb_const_get(parent, rb_intern(name)); rb_gc_register_mark_object(var); }
 
 void rbs__init_constants(void) {
-  IMPORT_CONSTANT(RBS_Types_Alias, RBS_Types, "Alias");
-  IMPORT_CONSTANT(RBS_Types_Bases_Any, RBS_Types_Bases, "Any");
-  IMPORT_CONSTANT(RBS_Types_Bases_Bool, RBS_Types_Bases, "Bool");
-  IMPORT_CONSTANT(RBS_Types_Bases_Bottom, RBS_Types_Bases, "Bottom");
-  IMPORT_CONSTANT(RBS_Types_Bases_Class, RBS_Types_Bases, "Class");
-  IMPORT_CONSTANT(RBS_Types_Bases_Instance, RBS_Types_Bases, "Instance");
-  IMPORT_CONSTANT(RBS_Types_Bases_Nil, RBS_Types_Bases, "Nil");
-  IMPORT_CONSTANT(RBS_Types_Bases_Self, RBS_Types_Bases, "Self");
-  IMPORT_CONSTANT(RBS_Types_Bases_Top, RBS_Types_Bases, "Top");
-  IMPORT_CONSTANT(RBS_Types_Bases_Void, RBS_Types_Bases, "Void");
-  IMPORT_CONSTANT(RBS_Types_Block, RBS_Types, "Block");
-  IMPORT_CONSTANT(RBS_Types_ClassInstance, RBS_Types, "ClassInstance");
-  IMPORT_CONSTANT(RBS_Types_ClassSingleton, RBS_Types, "ClassSingleton");
-  IMPORT_CONSTANT(RBS_Types_Function, RBS_Types, "Function");
-  IMPORT_CONSTANT(RBS_Types_UntypedFunction, RBS_Types, "UntypedFunction");
-  IMPORT_CONSTANT(RBS_Types_Function_Param, RBS_Types_Function, "Param");
-  IMPORT_CONSTANT(RBS_Types_Interface, RBS_Types, "Interface");
-  IMPORT_CONSTANT(RBS_Types_Intersection, RBS_Types, "Intersection");
-  IMPORT_CONSTANT(RBS_Types_Literal, RBS_Types, "Literal");
-  IMPORT_CONSTANT(RBS_Types_Optional, RBS_Types, "Optional");
-  IMPORT_CONSTANT(RBS_Types_Proc, RBS_Types, "Proc");
-  IMPORT_CONSTANT(RBS_Types_Record, RBS_Types, "Record");
-  IMPORT_CONSTANT(RBS_Types_Tuple, RBS_Types, "Tuple");
-  IMPORT_CONSTANT(RBS_Types_Union, RBS_Types, "Union");
-  IMPORT_CONSTANT(RBS_Types_Variable, RBS_Types, "Variable");
 }
--- ext/rbs_extension/constants.h
+++ ext/rbs_extension/constants.h
@@ -1,33 +1,6 @@
 #ifndef RBS__CONSTANTS_H
 #define RBS__CONSTANTS_H
 
-extern VALUE RBS_Types_Alias;
-extern VALUE RBS_Types_Bases;
-extern VALUE RBS_Types_Bases_Any;
-extern VALUE RBS_Types_Bases_Bool;
-extern VALUE RBS_Types_Bases_Bottom;
-extern VALUE RBS_Types_Bases_Class;
-extern VALUE RBS_Types_Bases_Instance;
-extern VALUE RBS_Types_Bases_Nil;
-extern VALUE RBS_Types_Bases_Self;
-extern VALUE RBS_Types_Bases_Top;
-extern VALUE RBS_Types_Bases_Void;
-extern VALUE RBS_Types_Block;
-extern VALUE RBS_Types_ClassInstance;
-extern VALUE RBS_Types_ClassSingleton;
-extern VALUE RBS_Types_Function_Param;
-extern VALUE RBS_Types_Function;
-extern VALUE RBS_Types_UntypedFunction;
-extern VALUE RBS_Types_Interface;
-extern VALUE RBS_Types_Intersection;
-extern VALUE RBS_Types_Literal;
-extern VALUE RBS_Types_Optional;
-extern VALUE RBS_Types_Proc;
-extern VALUE RBS_Types_Record;
-extern VALUE RBS_Types_Tuple;
-extern VALUE RBS_Types_Union;
-extern VALUE RBS_Types_Variable;
-
 void rbs__init_constants();
 
 #endif
