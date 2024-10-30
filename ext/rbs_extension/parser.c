dafb3e0867fc3aecc674f6d2636f53887de09ebc
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2024-10-30 16:28:52 -0400

    Remove `rbs_ast_new` for now
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- ext/rbs_extension/parser.c
+++ ext/rbs_extension/parser.c
@@ -2944,7 +2944,7 @@ parse_method_type_try(VALUE a) {
     parser_advance_assert(arg->parser, pEOF);
   }
 
-  return rbs_ast_new((rbs_node_t *)method_type);
+  return rbs_struct_to_ruby_value((rbs_node_t *)method_type);
 }
 
 static VALUE
@@ -2997,10 +2997,6 @@ rbsparser_lex(VALUE self, VALUE buffer, VALUE end_pos) {
   rbs_allocator_free(&allocator);
 
   return results;
-}
-
-VALUE rbs_ast_new(rbs_node_t *node) {
-  return node->cached_ruby_value;
 }
 
 void rbs__init_parser(void) {
