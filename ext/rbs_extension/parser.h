aa289e181b5a7df5e5fca9905208dc71dd6aaea9
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2024-10-30 16:16:37 -0400

    Try to remove some more VALUE
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- ext/rbs_extension/parser.h
+++ ext/rbs_extension/parser.h
@@ -9,8 +9,10 @@
  * */
 extern VALUE RBS_Parser;
 
+VALUE rbs_ast_new(rbs_node_t *node);
+
 VALUE parse_type(parserstate *state);
-VALUE parse_method_type(parserstate *state);
+rbs_methodtype_t *parse_method_type(parserstate *state);
 VALUE parse_signature(parserstate *state);
 
 void rbs__init_parser();
