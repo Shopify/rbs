c8276b68978888a6563368128a1fbcf0acf6f992
Alexander Momchilov <alexander.momchilov@shopify.com>
2024-10-31 19:48:10 -0400

    Handle `rbs_types_proc`

--- ext/rbs_extension/parser.c
+++ ext/rbs_extension/parser.c
@@ -740,8 +740,7 @@ static rbs_types_proc_t *parse_proc_type(parserstate *state) {
   parse_function(state, &function, &block, &proc_self);
   position end = state->current_token.range.end;
   VALUE loc = rbs_location_pp(state->buffer, &start, &end);
-  VALUE value = rbs_proc(function, block, loc, proc_self);
-  return rbs_types_proc_new(value, function, block, loc, proc_self);
+  return rbs_types_proc_new(function, block, loc, proc_self);
 }
 
 static void check_key_duplication(parserstate *state, VALUE fields, VALUE key) {
