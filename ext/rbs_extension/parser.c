4ae1cc20a3ddc3ea01b64280c3270b1ac12b53ee
Alexander Momchilov <alexander.momchilov@shopify.com>
2024-10-31 22:55:34 -0400

    Set types on `RBS::AST::Declarations::TypeAlias`

--- ext/rbs_extension/parser.c
+++ ext/rbs_extension/parser.c
@@ -1420,7 +1420,7 @@ rbs_ast_declarations_typealias_t *parse_type_decl(parserstate *state, position c
   parser_pop_typevar_table(state);
 
   VALUE comment = get_comment(state, comment_pos.line);
-  return rbs_ast_declarations_typealias_new(typename->base.cached_ruby_value, type_params->cached_ruby_value, type->cached_ruby_value, annotations->cached_ruby_value, location, comment);
+  return rbs_ast_declarations_typealias_new(typename, type_params, type, annotations, location, comment);
 }
 
 /*
