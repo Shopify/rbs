b335048c8ae12baf9b65b146e41feb7d4e34b7ed
Alexander Momchilov <alexander.momchilov@shopify.com>
2024-10-31 23:18:43 -0400

    Set types on `RBS::AST::Members::ClassVariable`

--- ext/rbs_extension/parser.c
+++ ext/rbs_extension/parser.c
@@ -1985,7 +1985,7 @@ rbs_node_t *parse_variable_member(parserstate *state, position comment_pos, rbs_
     rbs_loc_add_required_child(loc, rb_intern("colon"), colon_range);
     rbs_loc_add_optional_child(loc, rb_intern("kind"), kind_range);
 
-    return (rbs_node_t *)rbs_ast_members_classvariable_new(name, type->cached_ruby_value, location, comment);
+    return (rbs_node_t *) rbs_ast_members_classvariable_new(name, type, location, comment);
 
   case kSELF:
     kind_range.start = state->current_token.range.start;
