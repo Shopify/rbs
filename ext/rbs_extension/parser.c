130c47ea8a71d6362bbbb3780346a4bfe2b787a4
Alexander Momchilov <alexander.momchilov@shopify.com>
2024-10-31 21:02:30 -0400

    Handle `rbs_ast_members_methoddefinition`

--- ext/rbs_extension/parser.c
+++ ext/rbs_extension/parser.c
@@ -1753,17 +1753,7 @@ rbs_ast_members_methoddefinition_t *parse_member_def(parserstate *state, bool in
   rbs_loc_add_optional_child(loc, rb_intern("overloading"), overloading_range);
   rbs_loc_add_optional_child(loc, rb_intern("visibility"), visibility_range);
 
-  VALUE value = rbs_ast_members_method_definition(
-    name,
-    k,
-    overloads,
-    annotations->cached_ruby_value,
-    location,
-    comment,
-    overloading,
-    visibility
-  );
-  return rbs_ast_members_methoddefinition_new(value, name, k, overloads, annotations->cached_ruby_value, location, comment, overloading, visibility);
+  return rbs_ast_members_methoddefinition_new(name, k, overloads, annotations->cached_ruby_value, location, comment, overloading, visibility);
 }
 
 /**
