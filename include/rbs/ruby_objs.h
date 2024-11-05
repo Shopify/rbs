29a2538e01a05972b91760555510a7865dd95c51
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2024-11-05 14:22:47 -0500

    Migrate `RBS::AST::Annotation`
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- include/rbs/ruby_objs.h
+++ include/rbs/ruby_objs.h
@@ -10,7 +10,7 @@
 
 #include "ruby.h"
 
-VALUE rbs_ast_annotation(VALUE string, VALUE location);
+VALUE rbs_ast_annotation(VALUE string, rbs_location_t *location);
 VALUE rbs_ast_comment(VALUE string, VALUE location);
 VALUE rbs_ast_decl_class(rbs_typename_t *name, rbs_node_list_t *type_params, VALUE super_class, rbs_node_list_t *members, rbs_node_list_t *annotations, VALUE location, VALUE comment);
 VALUE rbs_ast_decl_class_super(VALUE name, rbs_node_list_t *args, VALUE location);
