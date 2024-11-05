c9b9d1f21f23158f548a4f3205a86bba0ecdb210
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2024-11-05 11:11:31 -0500

    Migrate `parse_type_params`
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- include/rbs/ast.h
+++ include/rbs/ast.h
@@ -390,8 +390,8 @@ typedef struct rbs_ast_symbol {
 typedef struct rbs_ast_typeparam {
     rbs_node_t base;
 
-    VALUE name;
-    VALUE variance;
+    struct rbs_ast_symbol *name;
+    struct rbs_ast_symbol *variance;
     VALUE upper_bound;
     VALUE default_type;
     VALUE unchecked;
@@ -633,7 +633,7 @@ rbs_ast_members_prepend_t *rbs_ast_members_prepend_new(rbs_allocator_t *allocato
 rbs_ast_members_private_t *rbs_ast_members_private_new(rbs_allocator_t *allocator, VALUE location);
 rbs_ast_members_public_t *rbs_ast_members_public_new(rbs_allocator_t *allocator, VALUE location);
 rbs_ast_symbol_t *rbs_ast_symbol_new(rbs_allocator_t *allocator, VALUE ruby_value);
-rbs_ast_typeparam_t *rbs_ast_typeparam_new(rbs_allocator_t *allocator, VALUE name, VALUE variance, VALUE upper_bound, VALUE default_type, VALUE unchecked, VALUE location);
+rbs_ast_typeparam_t *rbs_ast_typeparam_new(rbs_allocator_t *allocator, rbs_ast_symbol_t *name, rbs_ast_symbol_t *variance, VALUE upper_bound, VALUE default_type, VALUE unchecked, VALUE location);
 rbs_methodtype_t *rbs_methodtype_new(rbs_allocator_t *allocator, rbs_node_list_t *type_params, VALUE type, VALUE block, VALUE location);
 rbs_namespace_t *rbs_namespace_new(rbs_allocator_t *allocator, rbs_node_list_t *path, VALUE absolute);
 rbs_typename_t *rbs_typename_new(rbs_allocator_t *allocator, VALUE namespace, VALUE name);
