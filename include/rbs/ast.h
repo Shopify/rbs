adea58e55a00da3d2d159c2226bd84a483504f7d
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2024-11-05 12:15:41 -0500

    Migrate `parse_intersection`
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- include/rbs/ast.h
+++ include/rbs/ast.h
@@ -537,7 +537,7 @@ typedef struct rbs_types_interface {
 typedef struct rbs_types_intersection {
     rbs_node_t base;
 
-    VALUE types;
+    struct rbs_node_list *types;
     VALUE location;
 } rbs_types_intersection_t;
 
@@ -651,7 +651,7 @@ rbs_types_classsingleton_t *rbs_types_classsingleton_new(rbs_typename_t *name, V
 rbs_types_function_t *rbs_types_function_new(VALUE ruby_value, VALUE required_positionals, VALUE optional_positionals, VALUE rest_positionals, VALUE trailing_positionals, VALUE required_keywords, VALUE optional_keywords, VALUE rest_keywords, VALUE return_type);
 rbs_types_function_param_t *rbs_types_function_param_new(rbs_node_t *type, VALUE name, VALUE location);
 rbs_types_interface_t *rbs_types_interface_new(rbs_typename_t *name, rbs_node_list_t *args, VALUE location);
-rbs_types_intersection_t *rbs_types_intersection_new(VALUE types, VALUE location);
+rbs_types_intersection_t *rbs_types_intersection_new(rbs_node_list_t *types, VALUE location);
 rbs_types_literal_t *rbs_types_literal_new(VALUE literal, VALUE location);
 rbs_types_optional_t *rbs_types_optional_new(rbs_node_t *type, VALUE location);
 rbs_types_proc_t *rbs_types_proc_new(VALUE type, VALUE block, VALUE location, VALUE self_type);
