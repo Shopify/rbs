335926b848d15346f09899e1a535805b892592bc
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2024-11-05 15:08:43 -0500

    Invert `builds_ruby_object_internally` default value
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- templates/template.rb
+++ templates/template.rb
@@ -122,7 +122,7 @@ module RBS
         @c_type_enum_name = @c_base_name.upcase
 
         @expose_to_ruby = yaml.fetch("expose_to_ruby", true)
-        @builds_ruby_object_internally = yaml.fetch("builds_ruby_object_internally", false)
+        @builds_ruby_object_internally = yaml.fetch("builds_ruby_object_internally", true)
 
         @fields = yaml.fetch("fields", []).map { |field| Field.from_hash(field) }.freeze
 
