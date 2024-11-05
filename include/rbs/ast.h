49b7b3467f4bbf8dafece5846a06572551a4c7b3
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2024-11-05 12:05:09 -0500

    Migrate `rbs_ast_declarations_class_super_t`
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- include/rbs/ast.h
+++ include/rbs/ast.h
@@ -127,7 +127,7 @@ typedef struct rbs_ast_declarations_class_super {
     rbs_node_t base;
 
     VALUE name;
-    VALUE args;
+    struct rbs_node_list *args;
     VALUE location;
 } rbs_ast_declarations_class_super_t;
 
@@ -602,7 +602,7 @@ typedef struct rbs_types_variable {
 rbs_ast_annotation_t *rbs_ast_annotation_new(VALUE string, VALUE location);
 rbs_ast_comment_t *rbs_ast_comment_new(VALUE ruby_value, VALUE string, VALUE location);
 rbs_ast_declarations_class_t *rbs_ast_declarations_class_new(VALUE name, rbs_node_list_t *type_params, VALUE super_class, rbs_node_list_t *members, rbs_node_list_t *annotations, VALUE location, VALUE comment);
-rbs_ast_declarations_class_super_t *rbs_ast_declarations_class_super_new(VALUE name, VALUE args, VALUE location);
+rbs_ast_declarations_class_super_t *rbs_ast_declarations_class_super_new(VALUE name, rbs_node_list_t *args, VALUE location);
 rbs_ast_declarations_classalias_t *rbs_ast_declarations_classalias_new(rbs_typename_t *new_name, rbs_typename_t *old_name, VALUE location, VALUE comment);
 rbs_ast_declarations_constant_t *rbs_ast_declarations_constant_new(rbs_typename_t *name, rbs_node_t *type, VALUE location, VALUE comment);
 rbs_ast_declarations_global_t *rbs_ast_declarations_global_new(rbs_ast_symbol_t *name, rbs_node_t *type, VALUE location, VALUE comment);
