d855df35b2455416bfff3fdd72f94518c4f2e4e7
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2024-11-07 18:33:32 -0500

    Mark `_types_` as AST nodes
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- src/ast.c
+++ src/ast.c
@@ -2343,7 +2343,7 @@ VALUE rbs_struct_to_ruby_value(rbs_node_t *instance) {
             VALUE h = rb_hash_new();
             rb_hash_aset(h, ID2SYM(rb_intern("type_params")), rbs_node_list_to_ruby_array(node->type_params));
             rb_hash_aset(h, ID2SYM(rb_intern("type")), rbs_struct_to_ruby_value((rbs_node_t *) node->type)); // rbs_node
-            #warning unexpected type rbs_types_block            rb_hash_aset(h, ID2SYM(rb_intern("block")), rbs_struct_to_ruby_value((rbs_node_t *) node->block)); // rbs_types_block
+            rb_hash_aset(h, ID2SYM(rb_intern("block")), rbs_struct_to_ruby_value((rbs_node_t *) node->block)); // rbs_types_block
             rb_hash_aset(h, ID2SYM(rb_intern("location")), rbs_loc_to_ruby_location(node->location));
 
             return CLASS_NEW_INSTANCE(
@@ -2742,7 +2742,7 @@ VALUE rbs_struct_to_ruby_value(rbs_node_t *instance) {
             // [#<RBS::Template::Field name="type" c_type="rbs_node">, #<RBS::Template::Field name="block" c_type="rbs_types_block">, #<RBS::Template::Field name="location" c_type="rbs_location">, #<RBS::Template::Field name="self_type" c_type="rbs_node">]
             VALUE h = rb_hash_new();
             rb_hash_aset(h, ID2SYM(rb_intern("type")), rbs_struct_to_ruby_value((rbs_node_t *) node->type)); // rbs_node
-            #warning unexpected type rbs_types_block            rb_hash_aset(h, ID2SYM(rb_intern("block")), rbs_struct_to_ruby_value((rbs_node_t *) node->block)); // rbs_types_block
+            rb_hash_aset(h, ID2SYM(rb_intern("block")), rbs_struct_to_ruby_value((rbs_node_t *) node->block)); // rbs_types_block
             rb_hash_aset(h, ID2SYM(rb_intern("location")), rbs_loc_to_ruby_location(node->location));
             rb_hash_aset(h, ID2SYM(rb_intern("self_type")), rbs_struct_to_ruby_value((rbs_node_t *) node->self_type)); // rbs_node
 
