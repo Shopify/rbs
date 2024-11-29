281d30534061ec75d5123f06987f2075022c6fb0
Alexander Momchilov <alexander.momchilov@shopify.com>
2024-11-29 12:51:20 -0500

    Add `rbs_node_equal`

--- src/rbs_constant_pool.c
+++ src/rbs_constant_pool.c
@@ -44,3 +44,16 @@ rbs_constant_id_t rbs_constant_pool_insert_literal(rbs_constant_pool_t *self, co
 
     return rbs_constant_pool_insert_constant(self, start, strlen(start));
 }
+
+bool rbs_constant_id_equal(rbs_constant_pool_t *self, rbs_constant_id_t lhs, rbs_constant_id_t rhs) {
+    assert(self == fake_constant_pool);
+
+    if (lhs == rhs) return true;
+
+    rbs_constant_t *lhs_constant = rbs_constant_pool_id_to_constant(self, lhs);
+    rbs_constant_t *rhs_constant = rbs_constant_pool_id_to_constant(self, rhs);
+
+    if (lhs_constant->length != rhs_constant->length) return false;
+
+    return strncmp(lhs_constant->start, rhs_constant->start, lhs_constant->length) == 0;
+}
