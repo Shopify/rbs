c74feb96c227401b7e7f435365dd356135327c4d
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2024-11-29 17:26:41 -0500

    Set encoding in the lexer state
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- ext/rbs_extension/parserstate.c
+++ ext/rbs_extension/parserstate.c
@@ -295,6 +295,10 @@ lexstate *alloc_lexer(VALUE string, int start_pos, int end_pos) {
   lexer->start = lexer->current;
   lexer->first_token_of_line = lexer->current.column == 0;
 
+  rb_encoding *encoding = rb_enc_get(string);
+  const char *encoding_name = rb_enc_name(encoding);
+  lexer->encoding = rbs_encoding_find((const uint8_t *) encoding_name, (const uint8_t *) (encoding_name + strlen(encoding_name)));
+
   return lexer;
 }
 
