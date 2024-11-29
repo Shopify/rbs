2febd9576f0bad5b3c55788a650825403036a845
Alexander Momchilov <alexander.momchilov@shopify.com>
2024-11-29 14:11:24 -0500

    Remove `cached_ruby_object` from `rbs_location_t`

---
+++ include/rbs/rbs_location_internals.h
@@ -0,0 +1,25 @@
+#ifndef RBS__RBS_LOCATION_INTERNALS_H
+#define RBS__RBS_LOCATION_INTERNALS_H
+
+typedef struct {
+  int start;
+  int end;
+} rbs_loc_range;
+
+typedef struct {
+  ID name;
+  rbs_loc_range rg;
+} rbs_loc_entry;
+
+typedef unsigned int rbs_loc_entry_bitmap;
+
+// The flexible array always allocates, but it's okay.
+// This struct is not allocated when the `rbs_loc` doesn't have children.
+typedef struct {
+  unsigned short len;
+  unsigned short cap;
+  rbs_loc_entry_bitmap required_p;
+  rbs_loc_entry entries[1];
+} rbs_loc_children;
+
+#endif
