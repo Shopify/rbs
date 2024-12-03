38e8cee13be648a5ee332e5b482f2f0fa533d3e7
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2024-12-03 14:59:58 -0500

    Remove some checks from `rbs_location` (already done at translation time)
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- src/rbs_location.c
+++ src/rbs_location.c
@@ -1,26 +1,8 @@
 #include "rbs/rbs_location.h"
-#include "location.h"
+
+#include <stdlib.h>
 
 #define RBS_LOC_CHILDREN_SIZE(cap) (sizeof(rbs_loc_children) + sizeof(rbs_loc_entry) * ((cap) - 1))
-
-static void check_children_max(unsigned short n) {
-  size_t max = sizeof(rbs_loc_entry_bitmap) * 8;
-  if (n > max) {
-    rb_raise(rb_eRuntimeError, "Too many children added to location: %d", n);
-  }
-}
-
-static void check_children_cap(rbs_location_t *loc) {
-  if (loc->children == NULL) {
-    rbs_loc_alloc_children(loc, 1);
-  } else {
-    if (loc->children->len == loc->children->cap) {
-      check_children_max(loc->children->cap + 1);
-      size_t s = RBS_LOC_CHILDREN_SIZE(++loc->children->cap);
-      loc->children = realloc(loc->children, s);
-    }
-  }
-}
 
 static rbs_loc_range rbs_new_loc_range(range rg) {
   rbs_loc_range r = { rg.start.char_pos, rg.end.char_pos };
@@ -46,8 +28,6 @@ rbs_location_t *rbs_location_new(range rg) {
 }
 
 void rbs_loc_alloc_children(rbs_location_t *loc, int capacity) {
-  check_children_max(capacity);
-
   size_t s = RBS_LOC_CHILDREN_SIZE(capacity);
   loc->children = malloc(s);
 
@@ -64,8 +44,6 @@ void rbs_loc_add_required_child(rbs_location_t *loc, rbs_constant_id_t name, ran
 }
 
 void rbs_loc_add_optional_child(rbs_location_t *loc, rbs_constant_id_t name, range r) {
-  check_children_cap(loc);
-
   unsigned short i = loc->children->len++;
   loc->children->entries[i].name = name;
   loc->children->entries[i].rg = rbs_new_loc_range(r);
