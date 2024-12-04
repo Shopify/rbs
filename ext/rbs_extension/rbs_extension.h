ff908f1729ed5fc66e78115fe06746ff236c970c
Alexander Momchilov <alexander.momchilov@shopify.com>
2024-12-04 18:15:52 -0500

    Extract `raise_error_if_any()`

--- ext/rbs_extension/rbs_extension.h
+++ ext/rbs_extension/rbs_extension.h
@@ -11,12 +11,3 @@
  * RBS::Parser class
  * */
 extern VALUE RBS_Parser;
-
-/**
- * Raises RBS::ParsingError on `tok` with message constructed with given `fmt`.
- *
- * ```
- * foo.rbs:11:21...11:25: Syntax error: {message}, token=`{tok source}` ({tok type})
- * ```
- * */
-NORETURN(void) raise_error(VALUE buffer, error *error);
