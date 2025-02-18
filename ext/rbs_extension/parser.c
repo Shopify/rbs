f8574e44505abf1bbf0e3a258de006efd8bfb1e3
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2025-02-18 13:59:35 -0500

    Migrate RBS::AST::Declarations::Module
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>
    Co-authored-by: Alexander Momchilov <alexander.momchilov@shopify.com>

--- ext/rbs_extension/parser.c
+++ ext/rbs_extension/parser.c
@@ -2399,7 +2399,7 @@ static VALUE parse_module_members(parserstate *state) {
   module_decl ::= {module_name} module_type_params module_members <kEND>
                 | {module_name} module_name module_type_params `:` module_self_types module_members <kEND>
 */
-static VALUE parse_module_decl0(parserstate *state, range keyword_range, VALUE module_name, range name_range, VALUE comment, VALUE annotations) {
+static rbs_ast_declarations_module_t *parse_module_decl0(parserstate *state, range keyword_range, VALUE module_name, range name_range, VALUE comment, VALUE annotations) {
   parser_push_typevar_table(state, true);
 
   range decl_range;
@@ -2439,7 +2439,7 @@ static VALUE parse_module_decl0(parserstate *state, range keyword_range, VALUE m
 
   parser_pop_typevar_table(state);
 
-  return rbs_ast_decl_module(
+  return rbs_ast_declarations_module_new(
     module_name,
     type_params,
     self_types,
@@ -2488,7 +2488,7 @@ static VALUE parse_module_decl(parserstate *state, position comment_pos, VALUE a
 
     return rbs_ast_decl_module_alias(module_name, old_name, location, comment);
   } else {
-    return parse_module_decl0(state, keyword_range, module_name, module_name_range, comment, annotations);
+    return rbs_struct_to_ruby_value((rbs_node_t *)parse_module_decl0(state, keyword_range, module_name, module_name_range, comment, annotations));
   }
 }
 
--- src/ruby_objs.c
+++ src/ruby_objs.c
@@ -17,23 +17,6 @@
           rb_class_new_instance(argc, argv, receiver)
 #endif
 
-VALUE rbs_ast_decl_module(VALUE name, VALUE type_params, VALUE self_types, VALUE members, VALUE annotations, VALUE location, VALUE comment) {
-  VALUE _init_kwargs = rb_hash_new();
-  rb_hash_aset(_init_kwargs, ID2SYM(rb_intern("name")), name);
-  rb_hash_aset(_init_kwargs, ID2SYM(rb_intern("type_params")), type_params);
-  rb_hash_aset(_init_kwargs, ID2SYM(rb_intern("self_types")), self_types);
-  rb_hash_aset(_init_kwargs, ID2SYM(rb_intern("members")), members);
-  rb_hash_aset(_init_kwargs, ID2SYM(rb_intern("annotations")), annotations);
-  rb_hash_aset(_init_kwargs, ID2SYM(rb_intern("location")), location);
-  rb_hash_aset(_init_kwargs, ID2SYM(rb_intern("comment")), comment);
-
-  return CLASS_NEW_INSTANCE(
-    RBS_AST_Declarations_Module,
-    1,
-    &_init_kwargs
-  );
-}
-
 VALUE rbs_ast_decl_module_self(VALUE name, VALUE args, VALUE location) {
   VALUE _init_kwargs = rb_hash_new();
   rb_hash_aset(_init_kwargs, ID2SYM(rb_intern("name")), name);
