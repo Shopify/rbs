feafe295efb1ddec5fb96804162d62ed24c71be0
Max Bernstein <max@bernsteinbear.com>
2025-06-27 11:09:43 -0400

    Allocate the arena with malloc
    This allows for much faster parser allocations: RBS allocates one parser
    per sig.

--- src/util/rbs_allocator.c
+++ src/util/rbs_allocator.c
@@ -13,6 +13,7 @@
 #include <string.h> // for memset()
 #include <stdint.h>
 #include <inttypes.h>
+#include <stdio.h>
 
 #ifdef _WIN32
 #include <windows.h>
@@ -22,13 +23,9 @@
 #include <sys/mman.h>
 #endif
 
-#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__sun)
-#define MAP_ANONYMOUS MAP_ANON
-#endif
-
 struct rbs_allocator {
     uintptr_t heap_ptr;
-    uintptr_t size;
+    uintptr_t end;
 };
 
 static size_t get_system_page_size(void) {
@@ -43,41 +40,14 @@ static size_t get_system_page_size(void) {
 #endif
 }
 
-static void *map_memory(size_t size) {
-#ifdef _WIN32
-    LPVOID result = VirtualAlloc(NULL, size, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-    rbs_assert(result != NULL, "VirtualAlloc failed");
-#else
-    void *result = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    rbs_assert(result != MAP_FAILED, "mmap failed");
-#endif
-    return result;
-}
-
-static void destroy_memory(void *memory, size_t size) {
-#ifdef _WIN32
-    VirtualFree(memory, 0, MEM_RELEASE);
-#else
-    munmap(memory, size);
-#endif
-}
-
-static void guard_page(void *memory, size_t page_size) {
-#ifdef _WIN32
-    DWORD old_protect_;
-    BOOL result = VirtualProtect(memory, page_size, PAGE_NOACCESS, &old_protect_);
-    rbs_assert(result != 0, "VirtualProtect failed");
-#else
-    int result = mprotect(memory, page_size, PROT_NONE);
-    rbs_assert(result == 0, "mprotect failed");
-#endif
+static void destroy_memory(void *memory) {
+    free(memory);
 }
 
 static size_t rbs_allocator_default_mem(void) {
     size_t kib = 1024;
     size_t mib = kib * 1024;
-    size_t gib = mib * 1024;
-    return 4 * gib;
+    return 4 * mib;
 }
 
 static inline bool is_power_of_two(uintptr_t value) {
@@ -94,22 +64,18 @@ rbs_allocator_t *rbs_allocator_init(void) {
     size_t size = rbs_allocator_default_mem();
     size_t page_size = get_system_page_size();
     size = align(size, page_size);
-    void *mem = map_memory(size + page_size);
-    // Guard page; remove range checks in alloc fast path and hard fail if we
-    // consume all memory
-    void *last_page = (char *) mem + size;
-    guard_page(last_page, page_size);
+    void *mem = malloc(size);
     uintptr_t start = (uintptr_t) mem;
     rbs_allocator_t header = (rbs_allocator_t) {
         .heap_ptr = start + sizeof header,
-        .size = size + page_size,
+        .end = start + size,
     };
     memcpy(mem, &header, sizeof header);
     return (rbs_allocator_t *) mem;
 }
 
 void rbs_allocator_free(rbs_allocator_t *allocator) {
-    destroy_memory((void *) allocator, allocator->size);
+    destroy_memory((void *) allocator);
 }
 
 // Allocates `new_size` bytes from `allocator`, aligned to an `alignment`-byte boundary.
@@ -125,6 +91,10 @@ void *rbs_allocator_realloc_impl(rbs_allocator_t *allocator, void *ptr, size_t o
 void *rbs_allocator_malloc_impl(rbs_allocator_t *allocator, size_t size, size_t alignment) {
     rbs_assert(size % alignment == 0, "size must be a multiple of the alignment. size: %zu, alignment: %zu", size, alignment);
     uintptr_t aligned = align(allocator->heap_ptr, alignment);
+    if (aligned + size >= allocator->end) {
+        fprintf(stderr, "Out of memory. Please bump the total arena size (currently %zu) in rbs_allocator_default_mem.", rbs_allocator_default_mem());
+        abort();
+    }
     allocator->heap_ptr = aligned + size;
     return (void *) aligned;
 }
@@ -133,11 +103,7 @@ void *rbs_allocator_malloc_impl(rbs_allocator_t *allocator, size_t size, size_t 
 //       It's assumed that callers to this function will immediately write to the allocated memory, anyway.
 void *rbs_allocator_calloc_impl(rbs_allocator_t *allocator, size_t count, size_t size, size_t alignment) {
     void *p = rbs_allocator_malloc_many_impl(allocator, count, size, alignment);
-#if defined(__linux__)
-    // mmap with MAP_ANONYMOUS gives zero-filled pages.
-#else
     memset(p, 0, count * size);
-#endif
     return p;
 }
 
